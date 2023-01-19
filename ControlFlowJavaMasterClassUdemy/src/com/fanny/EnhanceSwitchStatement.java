ba7ce470aee582f30f05f75aeb20ec49b32d8e07
'Riezell' <'riezellfanny08@gmail.com'>
2023-01-19 13:49:14 +0100

    Commit3

---
+++ ControlFlowJavaMasterClassUdemy/src/com/fanny/EnhanceSwitchStatement.java
@@ -0,0 +1,23 @@
+package com.fanny;
+
+public class EnhanceSwitchStatement {
+
+	public static void main(String[] args) {
+		
+		String month = "OCTOBER";
+		System.out.println(month + " is in the " + getQuarter(month) +" quarter");
+	}
+	public static String getQuarter(String month) {
+		return switch (month) {
+		case "JANUARY", "FEBRUARY","MARCH" -> "1st";
+		case "APRIL", "MAY", "JUNE" -> "2nd";
+		case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+		case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+		default -> {
+			String badResponse = month + " is bad";
+			yield badResponse;
+		}
+		};
+	}
+
+}
